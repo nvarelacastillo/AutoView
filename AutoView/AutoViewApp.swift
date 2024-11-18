5534834d34c08f56865a0684a146b6fc2db82625
Nicole Varela <nicolevarela@nicoles-mbp-2.mynetworksettings.com>
2024-11-17 20:58:11 -0500

    First commit

---
+++ AutoView/AutoViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  AutoViewApp.swift
+//  AutoView
+//
+//  Created by Nicole Varela on 11/17/24.
+//
+
+import SwiftUI
+
+@main
+struct AutoViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
