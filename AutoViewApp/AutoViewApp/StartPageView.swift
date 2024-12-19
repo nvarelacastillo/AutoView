4b4e79488420d0ec593c695b64975ab282792020
Nicole Varela <nicolevarela@nicoles-mbp-2.mynetworksettings.com>
2024-12-19 15:44:44 -0500

    commit

--- AutoViewApp/AutoViewApp/StartPageView.swift
+++ AutoViewApp/AutoViewApp/StartPageView.swift
@@ -21,33 +21,15 @@ struct StartPageView: View {
                             .font(.headline)
                             .foregroundColor(.black)
                             .frame(width: 220, height: 50)
-                            .background(Color(UIColor(hex: "#FDFFB6")))
+                            .background(Color(red: 253 / 255, green: 255 / 255, blue: 182 / 255)) // Same as "#FDFFB6"
                             .cornerRadius(0)
                     }
-
                     .padding(.bottom, 230)
                 }
             }
         }
     }
 }
-
-extension UIColor {
-    convenience init(hex: String) {
-        let scanner = Scanner(string: hex)
-        if hex.hasPrefix("#") {
-            scanner.currentIndex = hex.index(after: hex.startIndex)
-        }
-        var rgbValue: UInt64 = 0
-        scanner.scanHexInt64(&rgbValue)
-        let red = CGFloat((rgbValue & 0xFF0000) >> 16) / 255.0
-        let green = CGFloat((rgbValue & 0x00FF00) >> 8) / 255.0
-        let blue = CGFloat(rgbValue & 0x0000FF) / 255.0
-        self.init(red: red, green: green, blue: blue, alpha: 1.0)
-    }
-}
-
-
 
 #Preview {
     StartPageView()
